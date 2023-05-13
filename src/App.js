9f4c27135177c7b679fe94a3ebdb8f01e8fe4256
siamalrafi <rahathasandipto@gmail.com>
2023-05-13 15:57:08 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-// import Navbar1 from "./Navbar1/Navbar1";
-import Navbar2 from "./Navbar2/Navbar2";
+import Navbar1 from "./Navbar1/Navbar1";
+// import Navbar2 from "./Navbar2/Navbar2";
 
 function App() {
    return (
       <div className="">
-         {/* <Navbar1 /> */}
-         <Navbar2 />
+         {/* up to down */}
+         <Navbar1 />
+         
+         {/* right side to left */}
+         {/* <Navbar2 /> */}
       </div>
    );
 }
--- src/Navbar1/README.md
+++ src/Navbar1/README.md
@@ -1,1 +1,4 @@
 # just need to install tailwind css for this navbar -It's fully responsive navbar for react.
+
+
+![Alt text](./up-to-down.png "Optional title")
