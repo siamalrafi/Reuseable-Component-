5aacaa226d971a5b9a8cec962bfaf1fce482b9f2
siamalrafi <rahathasandipto@gmail.com>
2023-05-13 15:58:34 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Navbar1 from "./Navbar1/Navbar1";
-// import Navbar2 from "./Navbar2/Navbar2";
+// import Navbar1 from "./Navbar/Navbar1/Navbar1";
+// import Navbar2 from "./Navbar/Navbar2/Navbar2";
 
 function App() {
    return (
       <div className="">
          {/* up to down */}
-         <Navbar1 />
+         {/* <Navbar1 /> */}
          
          {/* right side to left */}
          {/* <Navbar2 /> */}
